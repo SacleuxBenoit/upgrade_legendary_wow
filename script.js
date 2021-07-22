2651cc431276420b148e308243427cf370973bb1
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-07-23 00:41:59 +0200

    rename result to result_soulAsh :arrow_right: script.js

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ let wantedIlvl = document.getElementById('wantedIlvl');
 let showResult = document.getElementById('showResult');
 
 function calculateResult(){
-    let result = wantedIlvl.value - ownedIlvl.value;
-    showResult.textContent = 'soul ash : ' + result;
+    let result_soulAsh = wantedIlvl.value - ownedIlvl.value;
+    showResult.textContent = 'soul ash : ' + result_soulAsh;
 }
